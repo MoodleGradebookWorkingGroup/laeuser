53f89e0bc98a14ed7d228512eb85242874693670
Bob Puffer <puffro01@luther.edu>
2013-11-19 12:11:25 -0600

    changed one string to refer to leauser rather than user

--- lang/en/gradereport_laeuser.php
+++ lang/en/gradereport_laeuser.php
@@ -23,5 +23,5 @@
  */
 
 $string['pluginname'] = 'LAE User report';
-$string['user:view'] = 'View your own grade report';
+$string['laeuser:view'] = 'View your own grade report';
 $string['tablesummary'] = 'The table is arranged as a list of graded items including categories of graded items. When items are in a category they will be indicated as such.';
