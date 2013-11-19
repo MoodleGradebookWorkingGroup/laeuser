add01dc4feb8937053457801d4e084f6a800b800
Bob Puffer <puffro01@luther.edu>
2013-11-19 12:13:00 -0600

    commented out showaverage set default for showtotalsifcontainhidden to GRADE_REPORT_SHOW_TOTAL_IF_CONTAINS_HIDDEN,

--- settings.php
+++ settings.php
@@ -32,7 +32,7 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showfeedback', get_string('showfeedback', 'grades'), get_string('showfeedback_help', 'grades'), 1));
     $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showrange', get_string('showrange', 'grades'), get_string('showrange_help', 'grades'), 1));
     $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showweight', get_string('showweight', 'grades'), get_string('showweight_help', 'grades'), 0));
-    $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showaverage', get_string('showaverage', 'grades'), get_string('showaverage_help', 'grades'), 0));
+//    $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showaverage', get_string('showaverage', 'grades'), get_string('showaverage_help', 'grades'), 0));
     $settings->add(new admin_setting_configcheckbox('grade_report_laeuser_showlettergrade', get_string('showlettergrade', 'grades'), get_string('showlettergrade_help', 'grades'), 0));
     $settings->add(new admin_setting_configselect('grade_report_laeuser_rangedecimals', get_string('rangedecimals', 'grades'),
             get_string('rangedecimals_help', 'grades'), 0,array(0=>0, 1=>1, 2=>2, 3=>3, 4=>4, 5=>5)));
@@ -43,7 +43,7 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configselect('grade_report_laeuser_showhiddenitems', get_string('showhiddenitems', 'grades'), get_string('showhiddenitems_help', 'grades'), 1, $options));
 
     $settings->add(new admin_setting_configselect('grade_report_laeuser_showtotalsifcontainhidden', get_string('hidetotalifhiddenitems', 'grades'),
-                                                      get_string('hidetotalifhiddenitems_help', 'grades'), GRADE_REPORT_HIDE_TOTAL_IF_CONTAINS_HIDDEN,
+                                                      get_string('hidetotalifhiddenitems_help', 'grades'), GRADE_REPORT_SHOW_TOTAL_IF_CONTAINS_HIDDEN,
                                                       array(GRADE_REPORT_HIDE_TOTAL_IF_CONTAINS_HIDDEN => get_string('hide'),
                                                             GRADE_REPORT_SHOW_TOTAL_IF_CONTAINS_HIDDEN => get_string('hidetotalshowexhiddenitems', 'grades'),
                                                             GRADE_REPORT_SHOW_REAL_TOTAL_IF_CONTAINS_HIDDEN => get_string('hidetotalshowinchiddenitems', 'grades'))));
